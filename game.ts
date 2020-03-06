4e3bfbc9839d052bcdcb35d851b80792b25ba959
Frey413 <sfrazierka.student@waldorfmaui.org>
2020-03-06 07:01:25 -1000

    added command sequence

--- game.ts
+++ game.ts
@@ -1,13 +1,68 @@
 import {words} from "./words";
-//import inquirer from "inquirer";
+import * as Inquirer from "inquirer";
 
 const word: string = words[Math.floor(Math.random()*words.length)];
+const parts = word.split("");
+let letter;
+let correct = [""];
+let incorrect = [];
+let win: boolean = false;
 
-let stuff = "";
+main();
 
-for (let i = 0;i < word.length;i++){
-    stuff += "_ ";
+async function main() {
+    printCurrentState();
+    if (win) {
+        console.log("You win!");
+    }else if (incorrect.length > 4) {
+        console.log("You lost! Too many wrong guesses.");
+        console.log("The word was:" + word)
+    }else {
+        letter = await askForLetter();
+        checkGuess();
+        main();
+    }
 }
 
-console.log(stuff);
-console.log(word);
+function printCurrentState() {
+    
+    let stuff = "";
+    for (let i = 0;i < parts.length;i++) {
+        for (let n = 0;n < correct.length;n++) {
+            if (parts[i] == correct[n]) {
+                stuff += correct[n] + " ";
+                break;
+            }else if (n == correct.length - 1) {
+                stuff += "_ ";
+            }
+        }
+    }
+    console.log(stuff);
+    console.log("Wrong guesses: " + incorrect);
+
+    let splitStuff = stuff.split("");
+    for (let i = 0;i < splitStuff.length;i++) {
+        if (splitStuff[i] == "_") {
+            break;
+        }else if (i == splitStuff.length - 1){
+            win = true;
+        }
+    }
+
+}
+
+async function askForLetter() {
+    const answer = await Inquirer.prompt([{message: "Guess a letter", type: "input", name: "letter"}]);
+    return answer.letter;
+}
+
+function checkGuess() {
+    for (let i = 0;i < parts.length;i++) {
+        if (letter == parts[i]) {
+            correct.push(letter);
+            break;
+        }else if (parts.length - 1 == i){
+            incorrect.push(letter);
+        }
+    }
+}
